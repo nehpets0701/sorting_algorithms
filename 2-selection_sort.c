b629f10bd79f3889f7c759c9b44f25fdbcccd6fe
Ethan-23 <emanroberts@gmail.com>
2021-01-29 03:37:06 +0000

    Added task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int val, num;
+
+	for (i = 0; i < size; i++)
+	{
+		num = i;
+		val = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[num])
+			{
+				num = j;
+				val = array[num];
+			}
+		}
+		array[num] = array[i];
+		array[i] = val;
+		print_array(array, size);
+	}
+}
