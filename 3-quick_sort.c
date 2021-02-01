1aa7c270372c127d45def55b1964e1495997f2a0
nehpets <nehpets0701@gmail.com>
2021-02-01 20:36:07 +0000

    printing and working

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -19,7 +19,7 @@ void swap(int *a, int *b)
  * @low:lowest element
  * @high:highest element
  */
-int partition(int arr[], int low, int high)
+int partition(int arr[], int low, int high, size_t size)
 {
 	int pivot = arr[high];
 	int i = (low - 1);
@@ -31,9 +31,13 @@ int partition(int arr[], int low, int high)
 		{
 			i++;
 			swap(&arr[i], &arr[j]);
+			if (arr[i] != arr[j])
+				print_array(arr, size);
 		}
 	}
 	swap(&arr[i + 1], &arr[high]);
+	if (arr[i + 1] != arr[high])
+		print_array(arr, size);
 	return (i + 1);
 }
 
@@ -50,7 +54,7 @@ void sort(int *arr, int low, int high, size_t size)
 
 	if (low < high)
 	{
-		pIndex = partition(arr, low, high);
+		pIndex = partition(arr, low, high, size);
 		sort(arr, low, pIndex - 1, size);
 		sort(arr, pIndex + 1, high, size);
 	}
