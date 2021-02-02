038de86d90ea17af90656f576ef9bc668331983c
Ethan-23 <emanroberts@gmail.com>
2021-02-02 15:31:46 +0000

    Updated for advanced

--- sort.h
+++ sort.h
@@ -14,13 +14,22 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+        const int n;
+        struct listint_s *prev;
+        struct listint_s *next;
 } listint_t;
 
 
 /*prototypes*/
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+void cocktail_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
